dff8421cb4c2e11757582e9a48cda3d05aa5abd9
gurk1sh <Gustaf_99@live.se>
2022-03-06 13:31:52 +0100

    Clicker

--- CounterViewModel.js
+++ CounterViewModel.js
@@ -7,17 +7,39 @@ class CounterViewModel {
 
         var randomPerson = listOfNames[Math.floor(Math.random() * 10)];
 
+        self.clickPower = 1;
+        self.autoIncrement = 0;
         self.userName = ko.observable(randomPerson);
         self.counter = ko.observable(0);
 
         self.increment = function() {
-            self.counter(self.counter() + 1);
+            self.counter(self.counter() + self.clickPower);
         }
 
         self.decrement = function() {
-            self.counter(self.counter() - 1);
+            self.counter(self.counter() - self.clickPower);
+        }
+        
+        function incrementClicks() {
+            self.counter(self.counter() + self.autoIncrement);
         }
 
+        setInterval(incrementClicks, 1000);
+        
+        self.autoClicker = function(increment) {
+            switch (true) {
+                case (self.counter() >= 10 && self.counter() < 100): self.counter(self.counter()-10), self.autoIncrement += increment
+                break;
+                
+                case (self.counter() >= 100 && self.counter() < 1000): self.counter(self.counter()-100), self.autoIncrement += increment
+                break;
+                
+                case (self.counter() >= 1000): self.counter(self.counter()-1000), self.autoIncrement += increment
+                break;
+                
+                default: return
+             }
+        }
     }
 }
 
